c87920d7c41ac7e9a944d3d2843fdcd3ec24fb5c
JunHCha <quffl9612@naver.com>
2020-02-13 12:50:11 +0900

    Deque, Vector, Set, Map implemented

--- STL/STL/STL.cpp
+++ STL/STL/STL.cpp
@@ -1,8 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
+#include <string>
 #include <stack>
 #include <queue>
+#include <deque>
+#include <vector>
+#include <set>
+#include <map>
 
 using namespace std;
 
@@ -52,6 +57,69 @@ int main(void) {
 	}
 	// Priority Queue STL
 
+	deque<int> d;
+	cout << endl << endl << "Deque" << endl;
+	d.push_front(7); 
+	d.push_back(3);
+	d.pop_front();
+	d.push_front(2);
+	for (int i = 0; i < d.size(); i++) { cout << d[i] << "  "; }
+	cout << endl;
+	deque<int>::iterator iter;
+	iter = d.begin();
+	d.insert(iter + 1, 3, 5);
+	for (int i = 0; i < d.size(); i++) { cout << d[i] << "  "; }
+	cout << endl;
+	d.clear();
+	cout << d.empty();
+	//Deque STL
+
+	vector<int> v;
+	cout << endl << endl << "Vector" << endl;
+	v.push_back(3); v.push_back(5); v.push_back(7); v.push_back(9);
+	v.pop_back();
+	for (int i = 0; i < v.size(); i++) { cout << v[i] << "  "; }
+	cout << "\n";
+	vector<int>::iterator v_iter;
+	v_iter = v.begin();
+	v.insert(v_iter + 2, 3, 1);
+	for (int i = 0; i < v.size(); i++) { cout << v[i] << "  "; }
+	cout << "\n";
+	v.clear();
+	cout << v.empty() << endl;
+	// Vector STL
+
+	cout << endl << "Set" << endl;
+	int a[5] = { 10, 8, 6, 4, 2 };
+	set<int> se(a, a + 5);
+	set<int>::iterator s_iter = se.begin();
+	for (; s_iter != se.end(); s_iter++) {
+		cout << *s_iter << "  ";
+	}
+	cout << "\n";
+	se.insert(1); se.insert(3); se.insert(5); se.insert(7); se.insert(9);
+	for (s_iter = se.begin(); s_iter != se.end(); s_iter++) {
+		cout << *s_iter << "  ";
+	}
+	cout << "\n";
+	se.clear();
+	cout << se.empty() << "\n";
+	// Set STL
+
+	cout << endl << "Map" << endl;
+	map<string, int> m;
+	m["Cha Jun Hyung"] = 3; m["Hong Gil Dong"] = 1; m["JYP"] = 2;
+	map<string, int>::iterator m_iter = m.begin();
+	for (; m_iter != m.end(); m_iter++) {
+		cout << m_iter->first << ":" << m_iter->second << endl;
+	}
+	cout << m["unknown"] << "\n";
+	m["Add"] = 3;
+	m["unknown2"];
+	for (m_iter = m.begin(); m_iter != m.end(); m_iter++) {
+		cout << m_iter->first << ":" << m_iter->second << endl;
+	}
+	//Map STL
 
 	system("pause");
 }
